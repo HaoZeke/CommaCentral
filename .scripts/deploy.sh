51b2a6ec0a65abbd6e307acb41b49295f5be3078
HaoZeke <rohit@myarchbox.com>
2015-11-12 12:50:04 +0530

    test

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -8,9 +8,10 @@ cd ../_site
 #rm -rf .git
 
 git init
+git config user.name "HaoZeke"
+git config user.email "rohit@myarchbox.com"
 
-
-#git remote add upstream "git@github.com:HaoZeke/CommaCentral.git"
+git remote add upstream "https://$GH_TOKEN@github.com/HaoZeke/CommaCentral.git""
 git fetch upstream
 git reset upstream/gh-pages
 
