e5fbc26c6792e6615c838e91786a4947b82221a7
HaoZeke <rohit@myarchbox.com>
2015-11-13 21:33:13 +0530

    Prototyping

--- _plugins/textile.rb
+++ _plugins/textile.rb
@@ -1,7 +1,7 @@
 module Jekyll
   class Nohassle < Generator
     safe false
-    @@folder = '_textile
+    @@folder = '_textile'
 
     def generate(site)
         now = Date.today.strftime("%Y-%m-%d") 
@@ -10,11 +10,11 @@ module Jekyll
         old_text = File.read(f)
         n = f.dup
         n[0..@@folder.length] = ""
-        n = n.slice(0..-4)
+        n = "#{n.split("-")}".tr('[],""', '')
         File.open("temp.textile", 'w') do |file|
 
           file.write("---\nlayout: post\ncover: 'assets/images/cover_gen.jpg'\ntitle: #{n}\ndate: #{now}\ntags: generic\nsubclass: 'post tag-test tag-content'\ncategories: 'commacen'\nnavigation: True\nlogo: 'assets/images/cata.png'\n---\n")
-          file.write("h2. {n}\n")
+          file.write("h1. ")
           file.write(old_text)
         end
 
@@ -23,7 +23,7 @@ module Jekyll
         File.rename('temp.textile', f)
 
         # Renaming files with appropriate date
-        File.rename(f, "_posts/#{now}-#{File.basename(f.gsub(/\s+/, '-'))}") 
+        File.rename(f, "_posts/#{now}-#{File.basename(f.gsub(/\s+/, '-'))}.textile") 
       }
     end
   end
